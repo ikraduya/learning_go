203e1234b521469495d771d84781405f61374c48
ikraduya <ikraduya@gmail.com>
2026-02-13 00:45:16 +0700

    update ch10

---
+++ learning_go/ch10/exec/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	math "github.com/ikraduya/codingchallanges/go/learning_go/ch10"
+)
+
+func main() {
+	r := math.Add(1, 2)
+	fmt.Println(r)
+}
--- learning_go/ch10/math.go
+++ learning_go/ch10/math.go
@@ -1,4 +1,4 @@
-package ch10
+package math
 
 func Add(a, b int) int {
 	return a + b
